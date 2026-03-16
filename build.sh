b23fef54415768022dc928b97d00ace9aabda9dc
Renzo Yanpier Vasquez Chiclayo <100808604+RenzoVasquezChiclayo@users.noreply.github.com>
2026-03-16 15:22:55 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
-# instalar camoufox una sola vez en el build
+# descargar navegador camoufox
 python -m camoufox fetch
-
-# instalar OCR
-apt-get update
-apt-get install -y tesseract-ocr
