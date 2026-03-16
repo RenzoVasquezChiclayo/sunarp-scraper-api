23dc16da23a89fa1fe919392d606e800965d8e75
Renzo Yanpier Vasquez Chiclayo <100808604+RenzoVasquezChiclayo@users.noreply.github.com>
2026-03-16 15:56:51 -0500

    Update test_camoufox5.py

--- test_camoufox5.py
+++ test_camoufox5.py
@@ -8,6 +8,9 @@ import platform
 import shutil
 import pytesseract
 from PIL import Image
+print("CAMOUFOX_PATH:", os.environ.get("CAMOUFOX_PATH"))
+print("HOME:", os.environ.get("HOME"))
+print("Working dir:", os.getcwd())
 from camoufox.async_api import AsyncCamoufox
 
 # ── Configuración de Tesseract ──
